19eb8b9ed84bedbd94a4cc4d282eef7ab81c918a
mmosier-web <mmosier5@gmail.com>
2019-09-10 20:01:09 -0400

    first commit

---
+++ practicing/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace practicing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //I was using this as a template to learn loops and bools and how to break
+            Console.WriteLine("Guess a number between 1 and 10. If you guess the right number, you win. If you guess the wrong number, Try again.");
+
+            Console.WriteLine("guess a number");
+            while (true)
+            {
+                int guessedNumber = Convert.ToInt32(Console.ReadLine());
+
+                bool guess = true;
+
+                if (guessedNumber == 10 == true)
+                {
+                    Console.WriteLine("Lucky guess");
+
+                    break;
+                }
+                else if (guessedNumber > 10 == false)
+                {
+                    Console.WriteLine("Guess again...");
+                }
+                
+            }
+            
+           
+
+
+
+
+          
+             
+
+     
+            
+             
+            
+               
+          
+            
+         
+            
+
+
+            
+            
+            
+
+       
+
+            
+
+           
+            
+
+            
+            
+             
+
+            
+            
+
+            
+
+
+            
+            
+            
+        }
+              
+        }
+    }
